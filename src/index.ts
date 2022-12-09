e0231abac273be36cc55b0677e4d515d39937a76
Chris Allmark <chris.allmark@vodafone.com>
2022-12-09 17:16:42 +0000

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
 
 const argv = yargs(hideBin(process.argv))
-  .usage("Usage: $0 [-d number] [-p number]")
+  .usage("Usage: $0 [-d number]")
   .options({
     d: {
       type: "number",
       alias: "day",
     },
-    p: {
-      type: "number",
-      alias: "part",
-      choices: [1, 2],
-    },
   }).argv;
 
 if (argv.day) {
-  console.log(solutions[`day${argv.day}`].solve());
+  console.log(
+    `Solutions for Day${argv.day}: ${solutions[`day${argv.day}`].solve()}`
+  );
+} else {
+  for (let i = 1; i <= 25; i++) {
+    try {
+      console.log(`Solutions for Day ${i}: ${solutions[`day${i}`].solve()}`);
+    } catch (e) {
+      // not yet
+    }
+  }
 }
--- src/solutions/index.ts
+++ src/solutions/index.ts
@@ -1,5 +1,11 @@
 const day1 = require("./day1");
+const day2 = require("./day2");
 const day3 = require("./day3");
 const day4 = require("./day4");
+const day5 = require("./day5");
+const day6 = require("./day6");
+const day7 = require("./day7");
+const day8 = require("./day8");
+const day9 = require("./day9");
 
-module.exports = { day1, day3, day4 };
+module.exports = { day1, day2, day3, day4, day5, day6, day7, day8, day9 };
